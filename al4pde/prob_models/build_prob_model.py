caa0f43cf3dbf67750b9e54458f5a525859b6a6d
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-02-26 16:49:15 +0000

    Remove model argument.

--- al4pde/prob_models/build_prob_model.py
+++ al4pde/prob_models/build_prob_model.py
@@ -3,10 +3,10 @@ from al4pde.prob_models.PRE_model import build_PREModel
 
 def build_prob_model(task, cfg):
     print("DEBUG: cfg.prob_model =", cfg, flush=True)
-    
+
     if cfg._target_ == "al4pde.prob_models.ensemble.Ensemble":
         return build_ensemble(task, cfg)
     if cfg._target_ == "al4pde.prob_models.PRE_model.PREModel":
-        return build_PREModel(task, cfg.model_wrapper)
+        return build_PREModel(task, cfg)
     else:
         raise ValueError(f"Unknown prob_model target: {cfg._target_}")
