30e05185b8424699f4dd9b243d0ed4e2a00636bb
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-02-27 11:12:51 +0000

    Add model instantiation.

--- al4pde/prob_models/PRE_model.py
+++ al4pde/prob_models/PRE_model.py
@@ -4,6 +4,7 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 import torch.nn as nn
 import numpy as np
+from hydra.utils import instantiate
 
 from al4pde.evaluation.visualization import plot_worst_traj
 from al4pde.prob_models.prob_model import ProbModel
@@ -16,7 +17,7 @@ class PREModel(ProbModel):
 
     def __init__(self, task, model):
         super().__init__(task, model.training_type, model.t_train, model.batch_size, model.val_period, model.vis_period, model.loss)
-        self.model = model 
+        self.model = self.model = instantiate(model)
         self.stats.append(UncAvg("unc")) 
         self.stats.append(LossUncCorr("corr_unc_loss", self.loss))
     
@@ -46,18 +47,19 @@ class PREModel(ProbModel):
             return res
         else: 
             return res[...,1:-1,1:-1]
+    
 
     def train_single_epoch(self, current_epoch, total_epoch, num_epoch):
         self.model.train_single_epoch(current_epoch, total_epoch, num_epoch)
 
-    def train_n_epoch(self, al_iter: int, num_epoch: int, step_offset: int, vis: bool = True, prefix: str = "", is_last=False) -> float:
+    def train_n_epoch(self, al_iter: int, num_epoch: int, step_offset: int, vis: bool = True, prefix: str = "") -> float:
         total_time = 0
-        self.model.init_training(al_iter)
+        self.model.init_training(al_iter, load_train_data=True)
         for i in range(num_epoch):
             t = time.time()
             self.model.train_single_epoch(i, step_offset + i, num_epoch)
             total_time += time.time() - t
-            if i % self.model.val_period == 0:
+            if i % self.val_period == 0:
                 self.model.validate(step_offset + i, prefix=prefix)
             if vis and ((i > 0 and i % self.model.vis_period == 0) or i == num_epoch - 1):
                 self.model.visualize(step_offset + i)
