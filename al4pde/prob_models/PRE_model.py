aacb9153ad6b8b2efddeb1cce86b4a85662802a6
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-02-27 11:21:51 +0000

    Change config handling.

--- al4pde/prob_models/PRE_model.py
+++ al4pde/prob_models/PRE_model.py
@@ -15,9 +15,9 @@ from al4pde.prob_models.PREConvOps import ConvOps_1d
 
 class PREModel(ProbModel):
 
-    def __init__(self, task, model):
-        super().__init__(task, model.training_type, model.t_train, model.batch_size, model.val_period, model.vis_period, model.loss)
-        self.model = instantiate(model)
+    def __init__(self, task, model_cfg):
+        super().__init__(task, model_cfg.training_type, model_cfg.t_train, model_cfg.batch_size, model_cfg.val_period, model_cfg.vis_period, model_cfg.loss)
+        self.model = instantiate(model_cfg.model_wrapper)
         self.stats.append(UncAvg("unc")) 
         self.stats.append(LossUncCorr("corr_unc_loss", self.loss))
     
