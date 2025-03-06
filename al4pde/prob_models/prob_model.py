da088e4c88300207ad031820e727a95ec0e0e65c
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-06 11:33:51 +0000

    Print device.

--- al4pde/prob_models/prob_model.py
+++ al4pde/prob_models/prob_model.py
@@ -12,12 +12,14 @@ class ProbModel(Model):
         if self.training_type in ['autoregressive', 'teacher_forcing']:
             pred = xx
             unc_cell = [torch.zeros_like(xx)]
+            print("Device of unc ", unc_cell.device)
             for t in range(self.initial_step, final_step):
                 m, unc = self.uncertainty(xx, grid, t_idx, pde_param, True)
                 pred = torch.cat((pred, m), -2)
                 xx = torch.cat((xx[..., 1:, :], m), dim=-2)
                 unc_cell.append(unc)
                 print("Uncertainty shape ", unc.shape)
+                print("Device of unc ", unc_cell.device)
                 if t_idx is not None:
                     t_idx += 1
             return pred, torch.concat(unc_cell, dim=-2)
