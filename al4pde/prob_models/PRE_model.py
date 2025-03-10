81314f9bf4aa769e9bdb4111e58910bfba903abd
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-10 11:00:56 +0000

    Move calculation directly to unc_roll_out.

--- al4pde/prob_models/PRE_model.py
+++ al4pde/prob_models/PRE_model.py
@@ -154,7 +154,7 @@ class PREModel(ProbModel):
             print("Input field shape is ", xx.shape)
             pred = self.model.roll_out(xx, grid, final_step, pde_param, t_idx, return_features)
             print("Prediction shape is ", pred.shape)
-            unc = self.uncertainty(pred, grid, t_idx, pde_param, False)
+            unc = self.residual(pred, pde_param)
             print("Uncertainty shape is ", unc.shape)
 
             return pred, unc
