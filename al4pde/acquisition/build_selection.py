06ff85f59bd02672eef85d34224b3dfba1d58c4f
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-11 15:16:03 +0000

    Undo instantiation.

--- al4pde/acquisition/build_selection.py
+++ al4pde/acquisition/build_selection.py
@@ -6,5 +6,4 @@ def build_strategy(task, cfg):
     if cfg._target_ == "al4pde.acquisition.distance_pool_based.DistancePoolBased":
         return build_distance_pool_based(task, cfg)
     else:
-        cfg.data_schedule = hydra.utils.instantiate(cfg.data_schedule)
         return hydra.utils.instantiate(cfg, task=task)
