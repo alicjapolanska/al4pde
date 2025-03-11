9eb7c5aca1847871e36c93179e19d2fa9bd91290
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-11 15:57:20 +0000

    Instantiate another way

--- scripts/run_active_learning.py
+++ scripts/run_active_learning.py
@@ -92,9 +92,9 @@ def main(cfg: DictConfig):
         print(f"acquisition.data_schedule type: {type(cfg.acquisition.data_schedule)}")
         print(f"acquisition.data_schedule content: {cfg.acquisition.data_schedule}")
         if isinstance(cfg.acquisition.data_schedule, str):
-            cfg.acquisition.data_schedule = hydra.utils.instantiate(
-                OmegaConf.select(cfg, f"acquisition.data_schedule")
-            )
+            cfg.acquisition.data_schedule = OmegaConf.load(
+        f"config/acquisition/data_schedule/{cfg.acquisition.data_schedule}.yaml"
+    )
         print(f"acquisition.data_schedule type: {type(cfg.acquisition.data_schedule)}")
         print(f"acquisition.data_schedule content: {cfg.acquisition.data_schedule}")
 
