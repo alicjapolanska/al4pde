d173b610692013abbb31829640bd46fc564ebe4a
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-17 18:04:47 +0000

    Plot PRE comparison together

--- al4pde/prob_models/PRE_model.py
+++ al4pde/prob_models/PRE_model.py
@@ -22,6 +22,9 @@ class PREModel(ProbModel):
         self.model = model_cfg
         self.stats.append(UncAvg("unc")) 
         self.stats.append(LossUncCorr("corr_unc_loss", self.loss))
+        self.current_bad_predictions = {}
+        self.current_good_predictions = {}
+        self.current_ground_truths = {}
     
     def residual(self, uu, pde_param: float, boundary: bool = False, dx: float = 0.001, dt: float = 0.05):
         """Compute PRE residual for a rolled out solution u. Hardcoded to Burgers (for now).
@@ -105,14 +108,23 @@ class PREModel(ProbModel):
             if vis:
                 if (i > 0 and i % self.vis_period == 0) or i == num_epoch - 1:
                     self.visualize(step_offset + i)
-            if i==5:
+            if i==2:
                 if vis:
-                    print("Plotting PRE")
-                    self.visualize_PRE(add_to_label = "bad_" + str(al_iter))
+                    # Calculate prediction at second step
+                    print("Calculating bad model prediction")
+                    self.calculate_current_predictions(model_trained = False)
         
         if vis:
-            print("Plotting PRE")
-            self.visualize_PRE(add_to_label = "good_" + str(al_iter))
+            # Calculate prediction after training
+            print("Calculating good model prediction")
+            self.calculate_current_predictions(model_trained = False)
+            print("Plot PRE comparison over trajectories")
+            self.plot_PRE(add_to_label = "_" + str(al_iter))
+        
+            # Reset prediction to plot PRE for
+            self.current_bad_predictions = {}
+            self.current_good_predictions = {}
+            self.current_ground_truths = {}
         
         return total_time
         
@@ -134,9 +146,11 @@ class PREModel(ProbModel):
     def forward(self, xx, grid, pde_param=None, t_idx=None):
         return self.model(xx, grid, pde_param, t_idx)
 
-    def plot_PRE(self, yy, pde_param: float, save_label: str):
+    def plot_PRE(self, add_to_label: str = None):
         """Plot PRE as heatmaps for a data instance over all time steps.
-        Assumes trajectory has shape (1, Nx, Nt, 1)."""
+        Assumes trajectory has shape (1, Nx, Nt, 1).
+        
+            add_to_label (str): string that will be appended at the end of the plots' filenames"""
 
         PRE = self.residual(yy, pde_param)  # Compute residuals
         PRE = PRE.squeeze()  # Remove batch and channel dimensions
@@ -156,18 +170,79 @@ class PREModel(ProbModel):
         plt.savefig(os.path.join(self.task.img_save_path, save_label + ".png"))
         plt.show()
 
+        for data_idx in self.current_ground_truths:
 
-    def visualize_PRE(self, num_samples: int = 3, add_to_label: str = None):
-        """Randomly select num_samples validation instances and plot PRE for their trajectory, as well as rolled out trajectory by the model. The plot is just at time step save_step.
+            PRE_traj = self.residual(*self.current_ground_truths[data_idx]).squeeze()  # Compute residuals
+            PRE_before = self.residual(*self.current_bad_predictions[data_idx]).squeeze()  # Compute residuals
+            PRE_after = self.residual(*self.current_good_predictions[data_idx]).squeeze()  # Compute residuals
+
+            # Define x and t axes
+            Nx, Nt = PRE_before.shape
+            x_vals = np.arange(Nx)  # Spatial dimension
+            t_vals = np.arange(Nt)  # Time dimension
+            
+            # Set consistent color scale
+            vmin = min(PRE_before.min(), PRE_traj.min(), PRE_after.min())
+            vmax = max(PRE_before.max(), PRE_traj.max(), PRE_after.max())
+
+            # Create figure with 3 subplots
+            fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+            # Plot "Before Training" heatmap
+            im1 = axes[0].imshow(PRE_before, aspect='auto', origin='lower', cmap='coolwarm',
+                                extent=[t_vals.min(), t_vals.max(), x_vals.min(), x_vals.max()],
+                                vmin=vmin, vmax=vmax)
+            axes[0].set_xlabel("Time")
+            axes[0].set_ylabel("x")
+            axes[0].set_yticks([])
+            axes[0].set_title("PRE Before Training")
+
+            # Plot "Ground Truth" heatmap
+            im2 = axes[1].imshow(PRE_traj, aspect='auto', origin='lower', cmap='coolwarm',
+                                extent=[t_vals.min(), t_vals.max(), x_vals.min(), x_vals.max()],
+                                vmin=vmin, vmax=vmax)
+            axes[1].set_xlabel("Time")
+            axes[1].set_yticks([])
+            axes[1].set_title("Ground Truth PRE")
+
+            # Plot "After Training" heatmap
+            im3 = axes[2].imshow(PRE_after, aspect='auto', origin='lower', cmap='coolwarm',
+                                extent=[t_vals.min(), t_vals.max(), x_vals.min(), x_vals.max()],
+                                vmin=vmin, vmax=vmax)
+            axes[2].set_xlabel("Time")
+            axes[2].set_yticks([])
+            axes[2].set_title("PRE After Training")
+
+            # Add shared colorbar
+            cbar = fig.colorbar(im3, ax=axes, orientation='vertical', fraction=0.02)
+            cbar.set_label("PRE Value")
+
+            plt.tight_layout()
+
+            # Save and show plot
+            plt.savefig(os.path.join(self.task.img_save_path, "PRE_comp_" + str(data_idx) + add_to_label + ".png"))
+            plt.show()
+
+
+    def calculate_current_predictions(self, model_trained: bool, num_samples: int = 3):
+        """Randomly select num_samples validation instances and plot PRE for their trajectory, as well as rolled out trajectory by the model. 
+            The plot is just at time step save_step.
+
+            model_trained (bool): Whether model is trained or not. Determines if predictions are stored in 
+                current_bad_predictions (False) or current_good_predictions (True). If False, ground truths 
+                are also added to self.current_ground_truths.
 
             num_samples (int): number of trajectories to plot
-            add_to_label (str): string that will be appended at the end of the plots' filenames
             """
 
-        dataset_size = len(self.val_loader.dataset)  # Total number of samples
-        chosen_indices = set(random.sample(range(dataset_size), min(num_samples, dataset_size)))
+        if not self.model_trained:
+            dataset_size = len(self.val_loader.dataset)  # Total number of samples
+            chosen_indices = set(random.sample(range(dataset_size), min(num_samples, dataset_size)))
+        else:
+            chosen_indices = [*self.current_ground_truths] # Keys ie data indices as a list
 
         current_idx = 0  # Track global index in dataset
+        predictions = {}
 
         with torch.no_grad():
             for batch_idx, (xx, yy, grid, param, t_idx) in enumerate(self.val_loader):
@@ -175,15 +250,14 @@ class PREModel(ProbModel):
 
                 print("Shapes: batch_idx", batch_idx, " xx ", xx.shape, " yy ", yy.shape, " grid ", grid.shape, " param ", param.shape)
 
+
                 for i in range(batch_size):
                     if current_idx in chosen_indices:
                         sample_traj = yy[i, :, :, :].unsqueeze(0)  # Keep batch dimension
                         pde_param = param[i, :].item()
-                        save_label = f"PRE_val_sample_{current_idx}"
-                        if add_to_label:
-                            save_label += "_" + add_to_label
 
-                        self.plot_PRE(sample_traj, pde_param, save_label)
+                        if not model_trained:
+                            self.current_ground_truths[current_idx] = [sample_traj, pde_param]
 
                         print("Devices xx ", xx.device, " grid ", grid.device, " param ", param.device)
                         #plot rolled out timestep
@@ -194,13 +268,15 @@ class PREModel(ProbModel):
                         pred = self.model.roll_out(xx, grid, yy.shape[2], param, t_idx)[i, :, :, :].unsqueeze(0)
                         pred = pred.to("cpu")
 
-                        save_label = f"PRE_sample_rollout_{current_idx}"
-                        if add_to_label:
-                            save_label += "_" + add_to_label
-                        self.plot_PRE(pred, pde_param, save_label)
+                        predictions[current_idx] = [pred, pde_param]
+
 
                         chosen_indices.remove(current_idx)  # Remove so we stop early if needed
                         if not chosen_indices:  # Stop once we've processed all chosen indices
+                            if not model_trained:
+                                self.current_bad_predictions = predictions
+                            else:
+                                self.current_good_predictions = predictions
                             return
 
                     current_idx += 1  # Update global index across batches
