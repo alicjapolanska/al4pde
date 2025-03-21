9247a4454897e6e7865718880e0a1186ade104c9
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-21 15:10:40 +0000

    Use hydra to instantiate

--- scripts/plot_PRE_from_file.py
+++ scripts/plot_PRE_from_file.py
@@ -1,7 +1,12 @@
 import numpy as np
+import os
 import sys 
 
 sys.path.append('/leonardo/home/userexternal/apolansk/codes/pdearena')
+
+import hydra
+from omegaconf import DictConfig
+
 from al4pde.prob_models.PRE_model import PREModel
 from al4pde.utils import load_checkpoint
 from al4pde.prob_models.build_prob_model import build_prob_model
@@ -9,11 +14,21 @@ from al4pde.prob_models.build_prob_model import build_prob_model
 path_to_models = "data/runs/cnr1ahtx/checkpoints"
 num_al_iter = 4
 
+@hydra.main(version_base="1.3.2", config_path="../config", config_name="main")
+def main(cfg: DictConfig):
 
-model = PREModel()
+    run_id = cfg.checkpoint_id
+    run_save_path = os.path.join(cfg.task.run_save_path, run_id)
+    task = hydra.utils.instantiate(cfg.task, run_save_path=run_save_path)
+    model = build_prob_model(task, cfg.prob_model)
 
-for i in range(num_al_iter-1):
+    for i in range(num_al_iter-1):
 
-    load_checkpoint(path_to_models, str(i), model)
+        load_checkpoint(path_to_models, str(i), model)
 
-    print(model.current_ground_truths)
+        print(model.current_ground_truths)
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
