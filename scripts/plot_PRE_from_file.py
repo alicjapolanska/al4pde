b9f08bd963efc78be9dbfd6dcef3351a51c6f49e
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-21 14:21:19 +0000

    Change paths

--- scripts/plot_PRE_from_file.py
+++ scripts/plot_PRE_from_file.py
@@ -1,13 +1,10 @@
 import numpy as np
-import sys
-sys.path.append("/Users/alicjapolanska/Documents/ucl/ukaea/al4pde")
-print(sys.path)
 
 from al4pde.prob_models.PRE_model import PREModel
 from al4pde.utils import load_checkpoint
 from al4pde.prob_models.build_prob_model import build_prob_model
 
-path_to_models = "plots/cnr1ahtx/checkpoints"
+path_to_models = "data/runs/cnr1ahtx/checkpoints"
 num_al_iter = 4
 
 
