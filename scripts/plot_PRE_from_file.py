1aaf8d1f463d7f513b3245f8026ffb3360e74fe8
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-21 16:05:33 +0000

    Change path and filename

--- scripts/plot_PRE_from_file.py
+++ scripts/plot_PRE_from_file.py
@@ -81,7 +81,7 @@ def main(cfg: DictConfig):
     print("Prob model cfg", cfg.prob_model)
     prob_model = build_prob_model(task, cfg.prob_model)
 
-    first_al_iter, sampling_finished = load_checkpoint(run_save_path + "/checkpoints/"+str(al_iter)+".pt", cfg.checkpoint_file_name, prob_model)
+    first_al_iter, sampling_finished = load_checkpoint(run_save_path, str(al_iter)+".pt", prob_model)
 
     print(model.current_ground_truths)
 
