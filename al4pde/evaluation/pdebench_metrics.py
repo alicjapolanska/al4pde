23478ed172f4089b06fe57f1c67b8083ca6ed8b3
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-22 17:13:13 +0000

    Print shapes

--- al4pde/evaluation/pdebench_metrics.py
+++ al4pde/evaluation/pdebench_metrics.py
@@ -190,6 +190,8 @@ def metric_func(pred, target, if_mean=True, Lx=1., Ly=1., Lz=1., iLow=4, iHigh=1
         pred_no_B = pred[:,:,1:-1,1:-1,1:-1,1:-1]
         target_no_B = target[:,:,1:-1,1:-1,1:-1,1:-1]
 
+    print(pred.shape, target.shape, pred_no_B.shape, target_no_B.shape)
+
     err_mean_no_B = torch.sqrt(torch.mean((pred_no_B.view([nb, nc, -1, nt-2][:,:,]) - target_no_B.view([nb, nc, -1, nt-2])) ** 2, dim=2))  # mean over spatial dimensions
     err_RMSE_no_B = torch.mean(err_mean_no_B, dim=0)   # mean over batch dimension
 
