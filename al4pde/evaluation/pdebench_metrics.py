c35b0e6988f04bb417523408302e9fc50b987a8a
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-22 14:36:41 +0000

    Add RMSE without boundary

--- al4pde/evaluation/pdebench_metrics.py
+++ al4pde/evaluation/pdebench_metrics.py
@@ -179,6 +179,20 @@ def metric_func(pred, target, if_mean=True, Lx=1., Ly=1., Lz=1., iLow=4, iHigh=1
     nrm = torch.sqrt(torch.mean(target.view([nb, nc, -1, nt]) ** 2, dim=2))
     err_nRMSE = torch.mean(err_mean / nrm, dim=0)
 
+    #RMSE without boundary
+    if len(idxs) == 4:  # 1D
+        pred_no_B = pred_no_B[:,:,1:-1,1:-1]
+        target_no_B = target_no_B[:,:,1:-1,1:-1]
+    if len(idxs) == 5:  # 2D
+        pred_no_B = pred_no_B[:,:,1:-1,1:-1,1:-1]
+        target_no_B = target_no_B[:,:,1:-1,1:-1,1:-1]
+    elif len(idxs) == 6:  # 3D
+        pred_no_B = pred_no_B[:,:,1:-1,1:-1,1:-1,1:-1]
+        target_no_B = target_no_B[:,:,1:-1,1:-1,1:-1,1:-1]
+
+    err_mean_no_B = torch.sqrt(torch.mean((pred_no_B.view([nb, nc, -1, nt][:,:,]) - target_no_B.view([nb, nc, -1, nt])) ** 2, dim=2))  # mean over spatial dimensions
+    err_RMSE_no_B = torch.mean(err_mean_no_B, dim=0)   # mean over batch dimension
+
     err_CSV = torch.sqrt(torch.mean(
         (torch.sum(pred.view([nb, nc, -1, nt]), dim=2) - torch.sum(target.view([nb, nc, -1, nt]), dim=2)) ** 2,
         dim=0))    # sum over the spatial dimension followed by mean over batch dimension
@@ -268,4 +282,4 @@ def metric_func(pred, target, if_mean=True, Lx=1., Ly=1., Lz=1., iLow=4, iHigh=1
                torch.mean(err_BD, dim=[0, -1]), \
                torch.mean(err_F, dim=[0, -1])
     else:
-        return err_RMSE, err_nRMSE, err_CSV, err_Max, err_BD, err_F
+        return err_RMSE, err_nRMSE, err_CSV, err_Max, err_BD, err_F, err_RMSE_no_B
