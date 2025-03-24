40516fca56f4ba8f1a7180be4ee70664988a1e04
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-24 16:46:46 +0000

    Try loading in just model

--- scripts/plot_PRE_from_file.py
+++ scripts/plot_PRE_from_file.py
@@ -83,7 +83,7 @@ def main(cfg: DictConfig):
 
     save_dict = torch.load(path_to_models+str(num_al_iter)+".pt")
     prob_model.init_training(num_al_iter)
-    prob_model.load_state_dict(save_dict['model'])
+    prob_model.model.load_state_dict(save_dict['model'])
 
     print(prob_model.current_ground_truths)
 
