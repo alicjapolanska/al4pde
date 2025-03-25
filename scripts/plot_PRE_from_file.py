5ff2c5d709cebe95a0b8024b74e0232d25704d97
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-03-25 15:10:41 +0000

    Load in model dictionary

--- scripts/plot_PRE_from_file.py
+++ scripts/plot_PRE_from_file.py
@@ -20,46 +20,6 @@ from al4pde.utils import load_checkpoint, set_current_seed
 from al4pde.prob_models.build_prob_model import build_prob_model
 from al4pde.acquisition.build_selection import build_strategy
 
-if False:
-    path_to_models = "data/runs/cnr1ahtx"
-    num_al_iter = 4
-
-    @hydra.main(version_base="1.3.2", config_path="../config", config_name="main")
-    def main(cfg: DictConfig):
-
-        run_id = cfg.checkpoint_id
-        run_save_path = os.path.join(cfg.task.run_save_path, run_id)
-        print(run_save_path)
-        task = hydra.utils.instantiate(cfg.task, run_save_path=run_save_path)
-        model = build_prob_model(task, cfg.prob_model)
-
-        for i in range(num_al_iter-1):
-
-            load_checkpoint(path_to_models, str(i)+".pt", model)
-
-            print(model.current_ground_truths)
-
-
-    al_iter = 0
-    run = wandb.init(
-        project=cfg.wandb.project,
-        group=cfg.wandb.group,
-        name=cfg.wandb.name,
-        config=OmegaConf.to_container(cfg)
-    )
-
-    # init components
-    set_current_seed(cfg.seed, 0, sampling_finished=True, task=None, use_test=use_test)
-    run_save_path = os.path.join(cfg.task.run_save_path, run_id)
-    task = hydra.utils.instantiate(cfg.task, run_save_path=run_save_path)
-    acq_strat = build_strategy(task, cfg.acquisition)
-    print("acq", acq_strat, flush=True)
-
-    if __name__ == "__main__":
-        main()
-        print("Done")
-
-
 
 
 @hydra.main(version_base="1.3.2", config_path="../config", config_name="main")
@@ -84,8 +44,7 @@ def main(cfg: DictConfig):
     save_dict = torch.load(os.path.join(run_save_path, "checkpoints", str(num_al_iter)+".pt"))
     print("Dict keys ", save_dict['model'].keys())
     prob_model.init_training(num_al_iter)
-    print("Model keys ", prob_model.model.state_dict().keys())
-
+    build_prob_model.model.load_state_dict(save_dict['model'])
 
     print(prob_model.current_ground_truths)
 
