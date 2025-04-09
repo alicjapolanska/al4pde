8c3136c2045494950ce7ff07715dc736f1322368
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-04-09 16:15:31 +0100

    Add more printing.

--- al4pde/tasks/ic_gen/ic_gen_2d_ns_rand.py
+++ al4pde/tasks/ic_gen/ic_gen_2d_ns_rand.py
@@ -379,10 +379,11 @@ class ICGenNSRand(ICGenerator):
         u[:, 4, ...] = u[:, 4, ...] + d0[:, :, None, None] * T0[:, :, None, None] * (1.0 - mask[:, :, :, None])
         # u: [bs, 5, nx, ny, nz],   nz = 1 for 2D
         # for active learning requirement, remove z dimension and reformat to [bs, nx, ny, 5]
-        print("u shape is ", u.shape)
         u = u.squeeze(-1).permute(0, 2, 3, 1)  # [bs, nx, ny, 5])
+        print("u shape after squeezing is ", u.shape)
         # remove the extraneous Vz field since the ML model uses only 4 fields (DD, Vx, Vy, PP) for 2D
         u = u[:, :, :, (0, 1, 2, 4)].unsqueeze(-2)
+        print("u shape after unsqueezing is ", u.shape)
         if not torch.all(torch.isfinite(u)):
             for i in range(len(u)):
                 if not torch.all(torch.isfinite((u[i]))):
